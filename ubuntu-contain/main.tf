a397c44e69553b24dc03f9ec837398bb126f0583
tridibh <hazarika60@gmail.com>
2023-09-06 08:16:16 +0530

    update

--- ubuntu-contain/main.tf
+++ ubuntu-contain/main.tf
@@ -8,14 +8,13 @@ terraform {
 }
 provider "docker" {}
 
-resource "docker_image" "ubuntu" {
-  name = "ubuntu:precise"
+resource "docker_image" "nginx_image" {
+  name = "nginx:1.23.3"
 }
 
-resource "docker_container" "ubuntu" {
-  name  = "foo"
-  restart = "always"
-  image = docker_image.ubuntu.image_id
+resource "docker_container" "nginx_container" {
+  name  = "web-server"
+  image = docker_image.nginx_image.image_id
   ports {
     internal = 80
     external = 8080
